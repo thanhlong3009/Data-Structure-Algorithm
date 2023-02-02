a1c323650ad391fb7c5d4d10165fbf624dc4d141
thanhlong3009 <thanhlongite6@gmail.com>
2023-02-02 17:32:09 +0700

    final

--- leetcode_fighting/leetcode_fighting/src/LC485_MaxConsecutiveOnes.java
+++ leetcode_fighting/leetcode_fighting/src/LC485_MaxConsecutiveOnes.java
@@ -2,25 +2,24 @@
 
 public class LC485_MaxConsecutiveOnes {
     public static void main(String[] args) {
-        int[] rs = {1,1,0,1,1,1,0,1,1,1,1};
+        int[] rs = {1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1};
         System.out.println(findMaxConsecutiveOnes(rs));
     }
+
     public static int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0 ;
+        int max = 0;
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                count++;
-            } else if (nums[i] == 0 ) {
-                if(max < count) {
+            if (nums[i] == 0) {
+                count = 0;
+            } else {
+                count ++;
+                if (max < count) {
                     max = count;
-                    count = 0;
                 }
             }
         }
-        if(max < count) {
-            max = count;
-        }
+
         return max;
     }
 }
