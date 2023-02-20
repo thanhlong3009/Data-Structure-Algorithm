21ca9dd0591d88f0964d5c3ba70205ac56e14964
thanhlong3009 <thanhlongite6@gmail.com>
2023-02-20 21:54:31 +0700

    ud

--- leetcode_fighting/leetcode_fighting/src/day_07/LC283_MoveZeroes.java
+++ leetcode_fighting/leetcode_fighting/src/day_07/LC283_MoveZeroes.java
@@ -13,7 +13,7 @@ public class LC283_MoveZeroes {
     public static void moveZeroes(int[] nums) {
         int n = nums.length;  //2
         int idx = 0;           // 1
-        for (int i = 0; i < n; i++) { // 3n
+        for (int i = 0; i < n; i++) { // 6n
             if (nums[i] != 0) {
                 nums[idx] = nums[i];
                 idx++;
@@ -23,7 +23,7 @@ public class LC283_MoveZeroes {
             nums[idx] = 0;
             idx++;
         }
-        // -> sum = 3n + 2n - 2idx +3 = 5n -2idx + 3 > 3n + 3 <=> 3n ( n >= idx với mọi n)
+        // -> sum = 6n + 2n - 2idx +3 = 8n -2idx + 3 > 6n + 3 <=> 6n ( n >= idx với mọi n)
         // => độ phức tạp O(n)
     }
 }
