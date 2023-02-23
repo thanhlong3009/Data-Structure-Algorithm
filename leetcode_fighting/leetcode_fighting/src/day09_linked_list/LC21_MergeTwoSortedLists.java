69977497f61a51d96e8db89eb03d2700f198ccb6
thanhlong3009 <thanhlongite6@gmail.com>
2023-02-23 17:24:25 +0700

    ud

--- leetcode_fighting/leetcode_fighting/src/day09_linked_list/LC21_MergeTwoSortedLists.java
+++ leetcode_fighting/leetcode_fighting/src/day09_linked_list/LC21_MergeTwoSortedLists.java
@@ -19,6 +19,24 @@ public class LC21_MergeTwoSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
+        ListNode rs = new ListNode();
+        ListNode current = rs;
+
+        while (list1 != null && list2 != null){
+            if (list1.val < list2.val){
+                current.next = list1;
+                list1=list1.next;
+            }else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        if (list1 != null) {
+            current.next = list1;
+        }else {
+            current.next = list2;
+        }
+        return rs.next;
     }
 }
