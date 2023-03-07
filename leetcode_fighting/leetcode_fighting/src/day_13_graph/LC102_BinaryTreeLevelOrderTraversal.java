ae8af1db243237fb33d08458ae33fef9642b8854
thanhlong3009 <thanhlongite6@gmail.com>
2023-03-07 22:12:16 +0700

    ud

--- leetcode_fighting/leetcode_fighting/src/day_13_graph/LC102_BinaryTreeLevelOrderTraversal.java
+++ leetcode_fighting/leetcode_fighting/src/day_13_graph/LC102_BinaryTreeLevelOrderTraversal.java
@@ -1,11 +1,35 @@
 package day_13_graph;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LC102_BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
-
-        return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        if (root == null) {
+            return wrapList;
+        }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> subList = new LinkedList<>();
+            int LevelNum = queue.size();
+            for (int i = 0; i < LevelNum; i++) {
+                // If left and rigth Values Exist It will add them
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                // Add the First Element removes it
+                subList.add(queue.poll().val);
+            }
+            // Add Sub List Values in Main
+            wrapList.add(subList);
+        }
+        return wrapList;
     }
 
     public class TreeNode {
--- leetcode_fighting/leetcode_fighting/src/day_13_graph/LC797_AllPaths.java
+++ leetcode_fighting/leetcode_fighting/src/day_13_graph/LC797_AllPaths.java
@@ -1,9 +1,23 @@
 package day_13_graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LC797_AllPaths {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        return null;
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(graph,0,ans,new ArrayList<>());
+        return ans;
+    }
+    private void dfs(int[][] graph,int v,List<List<Integer>> ans,List<Integer> path) {
+        path.add(v);
+        if(v==graph.length-1) {
+            ans.add(new ArrayList<>(path));
+        } else {
+            for(int i=0;i<graph[v].length;i++) {
+                dfs(graph,graph[v][i],ans,path);
+            }
+        }
+        path.remove(path.size()-1);
     }
 }
